e2fee614c2225761465ba1e00b5fb8863c10f658
Jakob <red-rackham@jpost.ch>
2019-09-12 11:23:53 +0200

    minor fixes

--- app/src/main/java/ch/j2mb/matrisk/gameplay/controller/gameManager.kt
+++ app/src/main/java/ch/j2mb/matrisk/gameplay/controller/gameManager.kt
@@ -6,7 +6,7 @@ import ch.j2mb.matrisk.gameplay.model.Player
 
 
 /*This class is the Game manager who handles all the logic, process and communication with the players*/
-class gameManager (players: MutableList<Player>, continents: List<Continent>){
+class gameManager (var players: MutableList<Player>, var continents: List<Continent>){
 
     var round:Int = 0
     var gameOrder = mutableListOf<Player>()
